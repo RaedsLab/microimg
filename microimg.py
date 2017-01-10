b56215ecd63ee9d97c47b579fb3800c2e62d4ef1
Raed Chammam <raed.chammam@gmail.com>
2017-01-10 22:13:42 +0100

    added time it

--- microimg.py
+++ microimg.py
@@ -8,6 +8,7 @@ from io import BytesIO
 import io
 import binascii
 
+import timeit
 
 size = 42, 42
 
@@ -15,15 +16,19 @@ app = Flask(__name__)
 
 
 @app.route("/v0/", methods=['POST'])
-def hello():
+def v0():
     imageURL = request.values.get('image')
     if imageURL is None:
         return jsonify(status="error",
                        message="Image parameter required")
 
     try:
+        load_start_time = timeit.default_timer()
         sourceImage = requests.get(imageURL)
         img = Image.open(BytesIO(sourceImage.content))
+        load_elapsed = timeit.default_timer() - load_start_time
+
+        process_start_time = timeit.default_timer()
         output = io.BytesIO()
         img = img.resize(size, Image.ANTIALIAS)
         img.save(output, format='JPEG', optimize=True, quality=30)
@@ -33,17 +38,17 @@ def hello():
 
         # remove headers
         im_hex = im_hex.replace('ffd8ffe000104a46494600010100000100010000ffdb0043001b12141714111b1716171e1c1b2028422b28252528513a3d3042605565645f555d5b6a7899816a7190735b5d85b586909ea3abadab6780bcc9baa6c799a8aba4ffdb0043011c1e1e2823284e2b2b4ea46e5d6ea4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4a4ffc0001108002a002a03012200021101031101ffc400', '')
-
         im_base64 = im_hex.decode("hex").encode("base64")
+        process_elapsed = timeit.default_timer() - process_start_time
 
         return jsonify(status="ok",
-                       data=im_base64)
+                       data=im_base64,
+                       load_time=load_elapsed,
+                       process_time=process_elapsed)
 
     except IOError:
         return jsonify(status="error",
                        message="Error parsing image")
-
-    return "Hello World! " + str()
 
 if __name__ == "__main__":
     app.run()
